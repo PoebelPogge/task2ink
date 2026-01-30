603df7d9180b1c9534e4de3f317ab35aa5df1f05
Markus Poggenburg <pogge@MacBookPro.lan>
2026-01-30 19:33:58 +0100

    init

---
+++ backend/src/main/java/pge/solutions/task2ink/Task2InkApplication.java
@@ -0,0 +1,13 @@
+package pge.solutions.task2ink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task2InkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task2InkApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/pge/solutions/task2ink/Task2InkApplicationTests.java
@@ -0,0 +1,13 @@
+package pge.solutions.task2ink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task2InkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
